0450881c7517801c805a387c9f6f91e8e0a9d2c9
Milton Vafana <miltonhyndrex@gmail.com>
2025-04-24 16:26:19 +0200

    perf: SITE COMPLETE

---
+++ src/utils/megaMenu/downloads.ts
@@ -0,0 +1,38 @@
+export const downloadsMenu = [
+  {
+    sectionTitle: "Web Development",
+    items: [
+      {
+        icon: "badge",
+        title: "Website Development & Design",
+        description:
+          "We create stunning websites that captivate your audience and drive results.",
+        href: "#services",
+      },
+    ],
+  },
+  {
+    sectionTitle: "Other Services",
+    items: [
+      {
+        icon: "badge",
+        title: "Mobile App Development",
+        description: "We develop mobile applications that provide seamless user experiences.",
+        href: "#services",
+      },
+      {
+        icon: "badge",
+        title: "Game Development",
+        description: "We create engaging games that entertain and educate.",
+        href: "#services",
+      },
+      {
+        icon: "badge",
+        title: "Software Development",
+        description:
+          "We build custom software solutions tailored to your business needs.",
+        href: "#services",
+      },
+    ],
+  },
+];
---
+++ src/utils/megaMenu/platform.ts
@@ -0,0 +1,27 @@
+export const platformMenu = [
+  /* {
+    href: "/platform",
+    title: "Nia AI",
+    description: "A simple AI assistant for daily use.",
+    image: "datanova",
+  },
+  {
+    href: "/platform",
+    title: "InsightFlow Cloud",
+    description: "A web-based platform for collaborative data analytics.",
+    image: "if_cloud",
+  },
+  {
+    href: "/platform",
+    title: "InsightFlow Planner",
+    description: "Advanced forecasting and scenario planning for business strategy.",
+    image: "if_planner",
+  },
+  {
+    href: "/platform",
+    title: "InsightFlow Visualizer",
+    description:
+      "Custom dashboards and interactive data storytelling.",
+    image: "if_visualizer",
+  }, */
+];
