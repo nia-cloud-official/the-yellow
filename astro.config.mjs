a5f074518981fc648ddaa1e91e0bfbc844993446
Milton Vafana <miltonhyndrex@gmail.com>
2025-04-25 10:54:20 +0200

    modified:   astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
+// filepath: /home/earlvaltor/Desktop/the-yellow-main/astro.config.mjs
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import svelte from '@astrojs/svelte';
 import vercel from '@astrojs/vercel';
@@ -8,21 +8,9 @@ import vercel from '@astrojs/vercel';
 export default defineConfig({
   site: 'https://the-yellow-official.netlify.app', // Replace with your site's URL
   integrations: [
-    tailwind({
-      config: './tailwind.config.js', // Ensure this file exists
-    }),
     react(),
     svelte(),
   ],
   output: 'static', // Use 'static' for static hosting platforms like Netlify
   adapter: vercel(), // Use the Vercel adapter for deployment
-  vite: {
-    build: {
-      rollupOptions: {
-        output: {
-          manualChunks: undefined, // Optimize chunk splitting
-        },
-      },
-    },
-  },site
 });
